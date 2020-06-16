a3f6ed306fea82d148f82f072ae4b24b75fd63df
Luis Cabrita Vázquez <41645693+cabritavazquez@users.noreply.github.com>
2020-06-16 17:36:39 +0200

    Create new.php

---
+++ new.php
@@ -0,0 +1,67 @@
+<title>Add new book</title>
+<head>
+   <link rel = "stylesheet" type= "text/css" href="css/style.css">
+</head>
+
+<?php
+
+ 
+   $host = "127.0.0.1";
+   $username = "root";
+   $password = "Myp4ss00";
+   $database = "Web";
+   
+$conn = new mysqli ($host, $username, $password, $database);
+if ($conn->connect_errno) {
+    echo "Failed conex to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    echo $mysqli->host_info . "\n";
+}
+
+  $stmt = $conn->prepare("SELECT * from list where code=?");
+
+  $stmt->bind_param("s", $_GET['code']);
+
+  
+   $stmt->execute();
+   $rows = $stmt->get_result();
+   
+   if (!$rows)
+    die ($conn->error);
+   
+   $row=$rows->fetch_assoc();
+  ?>
+  <form method="post" action="/saveNew.php">
+
+  <h1>New book</h1>
+
+  <label for="code"><strong>Code:</strong></label>
+  <input type="text" name="code" id="code" value="<?php echo $row["code"]?$row["code"]:""?>"/> </br>
+
+  <label for="title"><strong>Title: </strong> </label>
+    <input type="text" name="title" id="title" value="<?php echo $row["title"]?$row["title"]:""?>"/>
+     </br>
+
+  <label for="author"><strong>Author: </strong> </label>
+    <input type="text" name="author" id="author" value="<?php echo $row["author"]?$row["author"]:""?>"/>
+     </br>
+
+  <label for="price"><strong>Price(€): </strong> </label>
+    <input type="text" name="price" id="price" value="<?php echo $row["price"]?$row["price"]:""?>"/>
+    </br>
+
+  <label for="genre"><strong>Genre: </strong> </label>
+    <input type="text" name="genre" id="genre" value="<?php echo $row["genre"]?$row["genre"]:""?>"/>
+      <br/><br/>
+
+
+  <input type="submit" value="Save changes">
+  <input type="reset" value="Retry">
+</form>
+
+<div class="uno">
+  <a href="/index.php">index</a>
+</div>
+
+<?php
+   $conn = null;
+  ?>
